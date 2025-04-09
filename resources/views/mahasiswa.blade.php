f5f0129fdedc47cfb0dc6b0dbcd643507b869106
AllMuchalif2 <almuchalifal@gmail.com>
2025-04-09 11:29:51 +0700

    pert4

--- resources/views/mahasiswa.blade.php
+++ resources/views/mahasiswa.blade.php
@@ -6,7 +6,7 @@
     {{-- <h1>Halaman Mahasiswa</h1> --}}
     @foreach ($data as $skripsi)
         <article class="mb-4">
-            <h5><a href="/mahasiswa/{{ $skripsi["slug"] }}">Judul : {{ $skripsi["title"] }}</a></h5>
+            <h5>Judul : <a href="/mahasiswa/{{ $skripsi["slug"] }}">{{ $skripsi["title"] }}</a></h5>
             
             <h6>{{ $skripsi["nim"] }} - {{ $skripsi["nama"] }}</h6>
             <p>isi : {{ $skripsi["isi"] }}</p>
