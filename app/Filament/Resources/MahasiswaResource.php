c174e504dfb7c47f92719ad6bda19b0718a058ba
AllMuchalif2 <almuchalifal@gmail.com>
2025-05-28 15:08:07 +0700

    pert-after-uts

---
+++ app/Filament/Resources/MahasiswaResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MahasiswaResource\Pages;
+use App\Filament\Resources\MahasiswaResource\RelationManagers;
+use App\Models\Mahasiswa;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+
+class MahasiswaResource extends Resource
+{
+    protected static ?string $model = Mahasiswa::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+                TextInput::make('nim'),
+                TextInput::make('nama'),
+                TextInput::make('progdi')
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('nim'),
+                TextColumn::make('nama'),
+                TextColumn::make('progdi')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMahasiswas::route('/'),
+            'create' => Pages\CreateMahasiswa::route('/create'),
+            'edit' => Pages\EditMahasiswa::route('/{record}/edit'),
+        ];
+    }
+}
--- app/Models/Mahasiswa.php
+++ app/Models/Mahasiswa.php
@@ -2,44 +2,10 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 
-class Mahasiswa 
+class Mahasiswa extends Model
 {
-    private static $skripsi = [
-        [
-            "title" => "Penerapan laravel pada...",
-            "slug" => "penerapan-laravel-pada",
-            "nim" => "23.230.0001",
-            "nama" => "lorem",
-            "isi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut corporis rem et tempore nemo unde dolorem facilis deserunt nisi totam.",
-        ],
-        [
-            "title" => "Penerapan Code Igniter pada...",
-            "slug" => "penerapan-codeigniter-pada",
-            "nim" => "23.230.0002",
-            "nama" => "ipsum",
-            "isi" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, qui!",
-        ]
-    
-    ];
-
-    public static function all()
-    {
-
-        return collect(self::$skripsi);
-    }
-
-    public static function find($slug)
-    {
-        $data = static::all();
-        /*
-        $new_skripsi = [];
-        foreach ($data as $isi) {
-            if ($isi['slug'] === $slug) {
-                $new_skripsi = $isi;
-            }
-        }
-            */
-        return $data->firstWhere('slug', $slug);
-    }
+    //
+    protected $guarded = [];
 }
